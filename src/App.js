5654148190e62f2ef7b994675e61c74d79c5b1bf
Ganeshan <ganeshanmadesh36@outlook.com>
2023-03-02 15:23:21 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import DropDown from "./components/DropDown";
+class App extends React.Component {
+  // Constructor
+  constructor(props) {
+    super(props);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    this.state = {
+      items: [],
+      DataisLoaded: false,
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+    fetch("https://restcountries.com/v2/region/asia")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true,
+        });
+      });
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded)
+      return (
+        <div>
+          <h1> Pleses wait some time.... </h1>{" "}
+        </div>
+      );
+    return (
+      <div className="App">
+        <h1>List of Countries</h1>
+        <DropDown />
+        <table
+          striped
+          bordered
+          hover
+          style={{
+            justifyContent: "center",
+            width: "80%",
+            margin: "auto",
+            marginTop: "30px",
+            marginBottom: "60px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <tbody>
+            <tr>
+              <th>Code</th>
+              <th>Name</th>
+              <th>Subregin</th>
+              <th>Flag</th>
+              <th>TimeZone</th>
+            </tr>
+            {items.map((item) => (
+              <tr item={item}>
+                <td>{item.numericCode}</td>
+                <td>{item.name}</td>
+                <td>{item.subregion}</td>
+                <td>
+                  <img
+                    width="20px"
+                    height="10px"
+                    src={item.flags.png}
+                    alt="flag"
+                  />
+                </td>
+                <td>{item.timezones}</td>
+                
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// return (
+//   <div className = "App">
+//     <h1> Region Data </h1>
+//     <DropDown/> {
+//       items.map((item) => (
+//       <ol key = { item.id } >
+//         Name: { item.name },
+//         Capital: { item.capital },
+//         Sub Region: { item.subregion }
+//         </ol>
+//       ))
+//     }
+//   </div>
+// );
